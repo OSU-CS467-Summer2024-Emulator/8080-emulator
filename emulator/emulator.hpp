6ce4170618b520cc4c5ecafe97454ea47fa6fa18
Michael Hunter <huntermi@oregonstate.edu>
2024-07-26 15:57:40 -0700

    Make Emulator class testable
    * Moved the Registers and Flags struct definitions out of the class
      definition so that other variables can be created with those types
      (specifically, in tests).
    * Moved the switch block from Emulate() to EmulateOpcode()
    * Added some getters to the Emulator class for use in tests.
    * At the top of test_em.cpp I overloaded the == operator for comparing
      Registers and Flags structs.
    * I wrote a few example tests to try things out.
    * Build was failing with a "emulator.cpp:162 undefined reference to
      `Disassembler::Disassemble(char*, int)`" error.
      I guess it wasn't linking the disassemble.cpp code with the
      emulator.cpp code. Adding target_link_libraries(Emulator Disassembler)
      to CMakeLists.txt in the emulator folder fixed this, but I doubt that
      is the best solution.

--- emulator/emulator.hpp
+++ emulator/emulator.hpp
@@ -4,6 +4,27 @@
 #include <string>
 #include <cstdint>
 
+typedef struct Registers
+{
+    unsigned char A = 0;
+    unsigned char B = 0;
+    unsigned char C = 0;
+    unsigned char D = 0;
+    unsigned char E = 0;
+    unsigned char H = 0;
+    unsigned char L = 0;
+} Registers;
+
+typedef struct Flags
+{
+    bool z = 0;  // zero
+    bool s = 0;  // sign
+    bool p = 0;  // parity
+    bool cy = 0; // carry
+    bool ac = 0; // auxiliary carry - not needed for space invaders
+} Flags;
+
+
 class Emulator
 {
     public:
@@ -17,6 +38,8 @@ class Emulator
         void ArithFlagsA(uint16_t res);
         void ZSPFlags(uint8_t value);
 
+        void SubtractFromA(uint8_t);
+
         void WriteToMem(uint16_t address, uint8_t value);
         uint8_t ReadFromHL();
         void WriteToHL(uint8_t value);
@@ -27,32 +50,20 @@ class Emulator
         void UnimplementedInstruction();
 
         void Emulate();
+        void EmulateOpcode(uint8_t);
 
         void PrintRegisters();
         void PrintFlags();
 
-        void SubtractFromA(uint8_t);
+        Registers GetRegisters();
+        Flags GetFlags();
+        int GetPC();
+        int GetSP();
 
     private:
-        struct Registers
-        {
-            unsigned char A = 0;
-            unsigned char B = 0;
-            unsigned char C = 0;
-            unsigned char D = 0;
-            unsigned char E = 0;
-            unsigned char H = 0;
-            unsigned char L = 0;
-        } registers;
+        Registers registers;
 
-        struct Flags
-        {
-            bool z = 0;  // zero
-            bool s = 0;  // sign
-            bool p = 0;  // parity
-            bool cy = 0; // carry
-            bool ac = 0; // auxiliary carry - not needed for space invaders
-        } flags;
+        Flags flags;
 
         // stack pointer
         int sp;
--- test/test_em.cpp
+++ test/test_em.cpp
@@ -4,7 +4,67 @@
 #include <catch2/catch_all.hpp>
 #include "emulator/emulator.hpp"
 
+bool operator==(const Registers &lhs, const Registers &rhs)
+{
+    return lhs.A == rhs.A && lhs.B == rhs.B && lhs.C == rhs.C && lhs.D == rhs.D &&
+           lhs.E == rhs.E && lhs.H == rhs.H && lhs.L == rhs.L;
+}
+
+bool operator==(const Flags &lhs, const Flags &rhs)
+{
+    return lhs.z == rhs.z && lhs.s == rhs.s && lhs.p == rhs.p &&
+           lhs.cy == rhs.cy && lhs.ac == rhs.ac;
+}
+
 TEST_CASE("A simple test", "[fast]")
 {
     REQUIRE(1 + 1 == 2);
+}
+
+TEST_CASE("parity", "[flag]")
+{
+    Emulator e;
+    REQUIRE(e.parity(0x00, 8) == 1);
+    REQUIRE(e.parity(0x01, 8) == 0);
+    REQUIRE(e.parity(0xf1, 8) == 0);
+    REQUIRE(e.parity(0xff, 8) == 1);
+}
+
+TEST_CASE("0x00 NOP", "[opcode]")
+{
+    Emulator e;
+    Registers registers_before = e.GetRegisters();
+    Flags flags_before = e.GetFlags();
+    int pc_before = e.GetPC();
+    int sp_before = e.GetSP();
+
+    e.EmulateOpcode(0x00);
+
+    REQUIRE(e.GetRegisters() == registers_before);
+    REQUIRE(e.GetFlags() == flags_before);
+    REQUIRE(e.GetPC() == pc_before + 1);
+    REQUIRE(e.GetSP() == sp_before);
+}
+
+// Random other example
+TEST_CASE("0x04 INR B", "[opcode]")
+{
+    Emulator e;
+    Registers registers_before = e.GetRegisters();
+    Flags flags_before = e.GetFlags();
+    int pc_before = e.GetPC();
+    int sp_before = e.GetSP();
+
+    e.EmulateOpcode(0x04);
+
+    Registers registers_after = registers_before;
+    registers_after.B++;
+
+    Flags flags_after;
+    flags_after.p = 1;
+
+    REQUIRE(e.GetRegisters() == registers_after);
+    REQUIRE(e.GetFlags() == flags_before);
+    REQUIRE(e.GetPC() == pc_before + 1);
+    REQUIRE(e.GetSP() == sp_before);
 }
